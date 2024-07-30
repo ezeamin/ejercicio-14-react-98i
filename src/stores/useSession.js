1f4cb8a1c2c2ae42e2018dc8bc2014c26b5f8272
Ezequiel Amin <ezequielamin@outlook.com>
2024-07-30 14:58:38 -0300

    agrego cambios de backend

--- src/stores/useSession.js
+++ src/stores/useSession.js
@@ -1,13 +1,24 @@
 import { create } from 'zustand';
+import { decodeJWT } from '../utilities/decodeJWT';
+
+let user = null;
+let isLoggedIn = false;
+
+const token = sessionStorage.getItem('token');
+if (token) {
+  user = decodeJWT(token).user;
+  isLoggedIn = true;
+}
 
 export const useSession = create((set) => {
   return {
-    user: null,
-    isLoggedIn: false,
+    user,
+    isLoggedIn,
     login: (newUser) => {
       set({ user: newUser, isLoggedIn: true });
     },
     logout: () => {
+      sessionStorage.removeItem('token');
       set({ user: null, isLoggedIn: false });
     },
   };
